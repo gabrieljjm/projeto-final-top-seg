3d5cc661787ef9c70a150e60345e5d842ff90cca
Gabriel <gaabriieelmiiraandaa@gmail.com>
2020-06-02 18:29:16 +0100

    Added InsertUser() for testing

--- Server/Server.cs
+++ Server/Server.cs
@@ -1,10 +1,12 @@
 ﻿using EI.SI;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,11 +18,16 @@ namespace Server
 		private static List<TcpClient> tcpClientList = new List<TcpClient>();
 		private static List<string> tcpNameList = new List<string>();
 		private static List<string> gameList = new List<string>();
+		private const int SALTSIZE = 8;
+		private const int NUMBER_OF_ITERATIONS = 50000;
+
 		//private static string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\logs-jogo-do-galo\\" + $@"log{DateTime.Now.Ticks}.txt";
 
 		private const int PORT = 10000;
 		static void Main(string[] args)
 		{
+			//InsertUser();
+
 			// CRIAR UM CONJUNTO IP+PORTA DO CLIENTE
 			IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, PORT);
 			// CRIAR UM TCP LISTENER
@@ -102,6 +109,83 @@ namespace Server
 				byte[] packet = protocolSI.Make(ProtocolSICmdType.DATA, msg);
 				networkStream.Write(packet, 0, packet.Length);
 			}
+		}
+
+		/// <summary>
+		/// Insert user in database just for testing
+		/// </summary>
+		public static void InsertUser()
+		{
+			string path = Directory.GetCurrentDirectory();
+			DirectoryInfo parentDir = Directory.GetParent(path);
+			Console.WriteLine(path);
+			//Alterar o finalpath = ""; para o caminho caso o programa não busque o caminho corretamente
+			string finalpath = path + "\\ServerDB.mdf";
+
+			string username = "Gabriel";//Alterar para inserir um username diferente (alterar sempre pois o username é UNIQUE)
+			string password = "123abc456";//Alterar para inserir uma password diferente
+			byte[] salt = GenerateSalt(SALTSIZE);
+			byte[] saltedPasswordHash = GenerateSaltedHash(password, salt);
+
+			SqlConnection conn;
+			try
+			{
+
+				// Configurar ligação à Base de Dados
+				conn = new SqlConnection();
+				conn.ConnectionString = string.Format(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='{0}';Integrated Security=True", finalpath);
+				//conn.ConnectionString = Properties.Settings.Default.connectionString;
+
+				// Abrir ligação à Base de Dados
+				conn.Open();
+
+				// Declaração dos parâmetros do comando SQL
+				SqlParameter paramUsername = new SqlParameter("@username", username);
+				SqlParameter paramPassHash = new SqlParameter("@saltedPasswordHash", saltedPasswordHash);
+				SqlParameter paramSalt = new SqlParameter("@salt", salt);
+
+				// Declaração do comando SQL
+				String sql = "INSERT INTO Users (Username, SaltedPasswordHash, Salt) VALUES (@username,@saltedPasswordHash,@salt)";
+
+				// Prepara comando SQL para ser executado na Base de Dados
+				SqlCommand cmd = new SqlCommand(sql, conn);
+
+				// Introduzir valores aos parâmentros registados no comando SQL
+				cmd.Parameters.Add(paramUsername);
+				cmd.Parameters.Add(paramPassHash);
+				cmd.Parameters.Add(paramSalt);
+
+				// Executar comando SQL
+				int lines = cmd.ExecuteNonQuery();
+
+				// Fechar ligação
+				conn.Close();
+				if (lines == 0)
+				{
+					// Se forem devolvidas 0 linhas alteradas então o não foi executado com sucesso
+					throw new Exception("Erro na inserção do utilizador");
+				}
+				//MessageBox.Show("Registado com sucesso!");
+			}
+			catch (Exception e)
+			{
+				throw new Exception("Erro na inserção do utilizador" + e.Message);
+			}
+		}
+
+		private static byte[] GenerateSalt(int size)
+		{
+			//Generate a cryptographic random number.
+			RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+			byte[] buff = new byte[size];
+			rng.GetBytes(buff);
+			return buff;
+		}
+
+		private static byte[] GenerateSaltedHash(string plainText, byte[] salt)
+		{
+			Rfc2898DeriveBytes rfc2898 = new Rfc2898DeriveBytes(plainText, salt, NUMBER_OF_ITERATIONS);
+			return rfc2898.GetBytes(32);
 		}
 	}
 }
